93d4727cc2f9f8be8d92e6bb2a3fd06a4be0d8fc
Luiz Gonzaga <luizgonzaga15@gmail.com>
2021-04-18 21:16:32 -0300

    Commit Inicial

---
+++ classes/Serie.cs
@@ -0,0 +1,43 @@
+namespace DIO.Series
+{
+    public class Serie : EntidadeBase
+    {
+        private Genero Genero { get; set; }
+
+        private string Titulo { get; set; }
+
+        private string Descricao { get; set; }
+
+        private int Ano { get; set; }
+
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero " + this.Genero + " | ";
+            retorno += "Título " + this.Titulo + " | ";
+            retorno += "Descrição " + this.Descricao + " | ";
+            retorno += "Ano " + this.Ano + ".";
+
+            return retorno;
+        } 
+
+        public string RetornaTitulo()
+        {
+            return this.Titulo;
+        }
+
+        public int RetornaId()
+        {
+             return this.Id;
+        }
+    }
+}
